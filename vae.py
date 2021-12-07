708a39c49030975bbfad29091488438968ea8f56
Felix Gravila <felixgravila@gmail.com>
2021-12-07 13:43:16 +0100

    :alembic: Add more experiments and attempts to improve"

--- vae.py
+++ vae.py
@@ -5,11 +5,14 @@ from collections import defaultdict
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorflow.keras.layers as L
-from tensorflow.python.keras.regularizers import L1
 from tqdm import tqdm
 import numpy as np
 
 from load_emojis import get_all_emojis
+from model import ResBlock
+from tqdm import tqdm
+
+from sklearn.manifold import TSNE
 
 #%%
 
@@ -39,17 +42,17 @@ else:
 
 # %%
 
-print("Taking the emoji datas and removing alpha channel")
-emoji_datas = [e.data[:, :, :3] for e in emojis]
+print("Taking the emoji datas, keeping the alpha channel")
+emoji_datas = [e.data[:, :, :] for e in emojis if int(e.desc.split("_")[0]) <= 942]
 
 print("Converting to tf.dataset")
 ds = tf.data.Dataset.from_tensor_slices(emoji_datas).shuffle(1000).batch(32)
 # %%
 
+LATENT_DIM = 16
+
 
 class Sampling(L.Layer):
-    """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
-
     def call(self, inputs):
         z_mean, z_log_var = inputs
         batch = tf.shape(z_mean)[0]
@@ -58,38 +61,95 @@ class Sampling(L.Layer):
         return z_mean + tf.exp(0.5 * z_log_var) * epsilon
 
 
-latent_dim = 20
+class Encoder(tf.keras.Model):
+    def __init__(self, latent_dim, input_shape, **kwargs):
+        super().__init__(**kwargs)
+        self._input_shape = input_shape
+        self.def_layers = [
+            ResBlock(8, "elu"),
+            L.MaxPool2D((2, 2)),
+            ResBlock(16, "elu"),
+            ResBlock(16, "elu"),
+            L.MaxPool2D((2, 2)),
+            ResBlock(32, "elu"),
+            ResBlock(32, "elu"),
+            L.Flatten(),
+            L.Dense(64, activation="elu"),
+            L.Dense(64, activation="elu"),
+            L.Dense(128, activation="elu"),
+        ]
+        self.z_mean = L.Dense(latent_dim, activation=None)
+        self.z_log_var = L.Dense(latent_dim, activation=None)
+        self.sampling = Sampling()
 
-encoder_inputs = tf.keras.Input(shape=(72, 72, 3))
-x = L.Conv2D(32, (3, 3), activation="relu", strides=2, padding="same")(encoder_inputs)
-x = L.Conv2D(32, (3, 3), activation="relu", strides=2, padding="same")(x)
-x = L.Conv2D(64, (3, 3), activation="relu", strides=2, padding="same")(encoder_inputs)
-x = L.Conv2D(64, (3, 3), activation="relu", strides=2, padding="same")(x)
-x = L.Flatten()(x)
-x = L.Dense(16, activation="relu")(x)
-z_mean = L.Dense(latent_dim, name="z_mean")(x)
-z_log_var = L.Dense(latent_dim, name="z_log_var")(x)
-z = Sampling()([z_mean, z_log_var])
-encoder = tf.keras.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
-encoder.summary()
+    def call(self, inputs):
+        x = inputs
+        for l in self.def_layers:
+            x = l(x)
 
-latent_inputs = tf.keras.Input(shape=(latent_dim,))
-x = L.Dense(18 * 18 * 64, activation="relu")(latent_inputs)
-x = L.Reshape((18, 18, 64))(x)
-x = L.Conv2DTranspose(64, (3, 3), activation="relu", strides=2, padding="same")(x)
-x = L.Conv2DTranspose(32, (3, 3), activation="relu", strides=2, padding="same")(x)
-decoder_outputs = L.Conv2DTranspose(3, (3, 3), activation="sigmoid", padding="same")(x)
-decoder = tf.keras.Model(latent_inputs, decoder_outputs, name="decoder")
-decoder.summary()
+        z_mean = self.z_mean(x)
+        z_log_var = self.z_log_var(x)
+        z = self.sampling([z_mean, z_log_var])
+        return [z_mean, z_log_var, z]
+
+    def summary(self):
+        input = L.Input(shape=self._input_shape)
+        output = self.call(input)
+        model = tf.keras.Model(inputs=input, outputs=output)
+        model.summary()
+
+
+enc = Encoder(LATENT_DIM, input_shape=(72, 72, 4))
+enc.build(input_shape=(None, 72, 72, 4))
+enc.summary()
+
+#%%
+
+
+class Decoder(tf.keras.Model):
+    def __init__(self, latent_dim, **kwargs):
+        super().__init__(**kwargs)
+        self._input_shape = (latent_dim,)
+        self.def_layers = [
+            L.Dense(128, activation="elu"),
+            L.Dense(128, activation="elu"),
+            L.Dense(10368, activation="elu"),
+            L.Reshape((18, 18, 32)),
+            L.Conv2DTranspose(64, (2, 2), activation="elu", strides=2),
+            ResBlock(64, "elu"),
+            L.Conv2DTranspose(32, (2, 2), activation="elu", strides=2),
+            ResBlock(32, "elu"),
+            ResBlock(16, "elu"),
+            L.Conv2D(4, (1, 1), activation="sigmoid"),
+        ]
+
+    def call(self, inputs):
+        x = inputs
+        for l in self.def_layers:
+            x = l(x)
+        return x
+
+    def summary(self):
+        input = L.Input(shape=self._input_shape)
+        output = self.call(input)
+        model = tf.keras.Model(inputs=input, outputs=output)
+        model.summary()
+
+
+dec = Decoder(latent_dim=16)
+dec.build(input_shape=(None, 16))
+dec.summary()
 
 # %%
 
 
 class VAE(tf.keras.Model):
-    def __init__(self, encoder, decoder, **kwargs):
-        super(VAE, self).__init__(**kwargs)
-        self.encoder = encoder
-        self.decoder = decoder
+    def __init__(self, latent_dim, input_shape, kl_loss_epsilon=0.1, **kwargs):
+        super().__init__(**kwargs)
+        self.kl_loss_epsilon = kl_loss_epsilon
+        self._input_shape = input_shape
+        self.encoder = Encoder(latent_dim, input_shape, **kwargs)
+        self.decoder = Decoder(latent_dim, **kwargs)
         self.total_loss_tracker = tf.keras.metrics.Mean(name="total_loss")
         self.reconstruction_loss_tracker = tf.keras.metrics.Mean(
             name="reconstruction_loss"
@@ -104,18 +164,25 @@ class VAE(tf.keras.Model):
             self.kl_loss_tracker,
         ]
 
+    def call(self, inputs):
+        z_mean, z_log_var, z = self.encoder(inputs)
+        reconstruction = self.decoder(z)
+        return z_mean, z_log_var, z, reconstruction
+
     def train_step(self, data):
         with tf.GradientTape() as tape:
-            z_mean, z_log_var, z = self.encoder(data)
-            reconstruction = self.decoder(z)
+            z_mean, z_log_var, z, reconstruction = self.call(data)
             reconstruction_loss = tf.reduce_mean(
                 tf.reduce_sum(
                     tf.keras.losses.binary_crossentropy(data, reconstruction),
                     axis=(1, 2),
                 )
             )
+            # reconstruction_loss = tf.reduce_sum(tf.losses.mse(data, reconstruction))
             kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
-            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
+            kl_loss = (
+                tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1)) * self.kl_loss_epsilon
+            )
             total_loss = reconstruction_loss + kl_loss
         grads = tape.gradient(total_loss, self.trainable_weights)
         self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
@@ -128,11 +195,53 @@ class VAE(tf.keras.Model):
             "kl_loss": self.kl_loss_tracker.result(),
         }
 
+    # def build(self, batch_size=None):
+    #     return super().build([batch_size] + list(self._input_shape))
+
 
 # %%
 
-vae = VAE(encoder, decoder)
-vae.compile(optimizer=tf.keras.optimizers.Adam())
-vae.fit(ds, epochs=200)
+vae = VAE(latent_dim=LATENT_DIM, input_shape=(72, 72, 4), kl_loss_epsilon=0.00001)
+vae.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001))
 
+#%%
+
+
+vae.fit(
+    ds,
+    epochs=300,
+    callbacks=[
+        tf.keras.callbacks.ReduceLROnPlateau(monitor="loss", factor=0.75, patience=5)
+    ],
+)
+
+# %%
+
+x = next(iter(ds))
+fig = plt.figure(figsize=(10, 20), facecolor="white")
+for i, e in enumerate(x[:4]):
+    orig = fig.add_subplot(4, 2, (i * 2) + 1)
+    reconst = fig.add_subplot(4, 2, (i * 2) + 2)
+    orig.imshow(e)
+    _, _, z = vae.encoder(np.array([e]))
+    r = vae.decoder(z)
+    reconst.imshow(r[0])
+
+
+# %%
+
+encodings = []
+ds = tf.data.Dataset.from_tensor_slices(emoji_datas).batch(128)
+for e in tqdm(ds):
+    _, _, z = vae.encoder(e)
+    encodings.extend(z)
+
+
+# %%
+
+
+X = TSNE(n_components=2, learning_rate="auto", init="random").fit_transform(
+    np.array(encodings)
+)
+plt.scatter(X[:, 0], X[:, 1])
 # %%
